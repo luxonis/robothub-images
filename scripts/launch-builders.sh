6338cbe872f396abbfe38716c6e01e2fab9ee754
Filip Prochazka <filip@thekingdom.nz>
2022-05-24 12:50:43 +0200

    fix aws install

--- scripts/launch-builders.sh
+++ scripts/launch-builders.sh
@@ -3,7 +3,7 @@ set -e
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-sudo ./aws/install
+sudo ./aws/install --update
 
 sudo apt-get update
 sudo apt-get install -y jq
