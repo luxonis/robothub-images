38246a48b63aebdfd3d0ad7cf88c8ac291d37f77
Filip Prochazka <filip@thekingdom.nz>
2022-05-24 23:58:36 +0200

    switch back to ubuntu

--- scripts/setup-remote-builder.sh
+++ scripts/setup-remote-builder.sh
@@ -12,26 +12,28 @@ ssh-add ~/.ssh/aws_key
 touch ~/.ssh/config
 echo "Host ${X86_BUILDER_IP}" >> ~/.ssh/config
 echo "  HostName ${X86_BUILDER_IP}" >> ~/.ssh/config
-echo "  User root" >> ~/.ssh/config
+echo "  User ubuntu" >> ~/.ssh/config
 echo "  StrictHostKeyChecking no" >> ~/.ssh/config
 echo "  IdentityFile /home/runner/.ssh/aws_key" >> ~/.ssh/config
 echo "Host ${ARM_BUILDER_IP}" >> ~/.ssh/config
 echo "  HostName ${ARM_BUILDER_IP}" >> ~/.ssh/config
-echo "  User root" >> ~/.ssh/config
+echo "  User ubuntu" >> ~/.ssh/config
 echo "  StrictHostKeyChecking no" >> ~/.ssh/config
 echo "  IdentityFile /home/runner/.ssh/aws_key" >> ~/.ssh/config
 
+echo "configuring X86 builder at ${X86_BUILDER_IP}"
 docker buildx create \
   --name remotebuilder \
   --node amd64 \
   --platform linux/amd64,linux/386 \
-  ssh://root@${X86_BUILDER_IP}
+  "ssh://ubuntu@${X86_BUILDER_IP}"
 
+echo "configuring ARM builder at ${ARM_BUILDER_IP}"
 docker buildx create \
   --name remotebuilder \
   --append \
   --node arm64 \
   --platform linux/arm64,linux/arm/v7,linux/arm/v6 \
-  ssh://root@${ARM_BUILDER_IP}
+  "ssh://ubuntu@${ARM_BUILDER_IP}"
 
 docker buildx inspect --bootstrap --builder remotebuilder
