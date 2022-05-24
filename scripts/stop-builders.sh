7988f709d99cb99902203e043f7950209bd97d22
Filip Prochazka <filip@thekingdom.nz>
2022-05-24 15:10:43 +0200

    fix stop builders

--- scripts/stop-builders.sh
+++ scripts/stop-builders.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-aws terminate --instance-id $AWS_X86_BUILDER_ID
-aws terminate --instance-id $AWS_ARM_BUILDER_ID
+aws ec2 terminate-instances --instance-ids "${AWS_X86_BUILDER_ID}" "${$AWS_ARM_BUILDER_ID}"
