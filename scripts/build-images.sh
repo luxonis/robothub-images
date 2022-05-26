8f64b40cc9e6d3f956407c54f3a541d87e2afb97
Filip Prochazka <filip@thekingdom.nz>
2022-05-26 08:41:40 +0200

    improve build naming

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -4,8 +4,18 @@ set -Eeuo pipefail
 if [[ -z "${DEPTHAI_BRANCH}" ]]; then
   DEPTHAI_BRANCH="main"
 fi
-ALPINE_TAG="ghcr.io/luxonis/robothub-base-app:alpine-depthai-${DEPTHAI_BRANCH}"
-UBUNTU_TAG="ghcr.io/luxonis/robothub-base-app:ubuntu-depthai-${DEPTHAI_BRANCH}"
+
+commitSuffix=""
+if [[ "$GITHUB_REF_NAME" != "main" ]]; then
+  if [[ "$GITHUB_REF_NAME" == "develop" ]]; then
+    commitSuffix="-develop"
+  else
+    commitSuffix="-${GITHUB_SHA}"
+  fi
+fi
+
+ALPINE_TAG="ghcr.io/luxonis/robothub-base-app:alpine-depthai-${DEPTHAI_BRANCH}${commitSuffix}"
+UBUNTU_TAG="ghcr.io/luxonis/robothub-base-app:ubuntu-depthai-${DEPTHAI_BRANCH}${commitSuffix}"
 LATEST_TAG="ghcr.io/luxonis/robothub-base-app:latest"
 
 echo "Building alpine..."
@@ -32,7 +42,7 @@ DOCKER_BUILDKIT=1 docker buildx \
   --file ./robothub_sdk/docker/ubuntu/Dockerfile \
   ./robothub_sdk
 
-if [[ "${DEPTHAI_BRANCH}" == "main" ]]; then
+if [[ "${DEPTHAI_BRANCH}" == "main" && "${GITHUB_REF_NAME}" == "main" ]]; then
   docker tag "$ALPINE_TAG" "$LATEST_TAG"
   docker push "$LATEST_TAG"
 fi
