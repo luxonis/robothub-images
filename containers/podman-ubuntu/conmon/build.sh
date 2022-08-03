f5c7c82e8faebadfff58fab4f066de7f739b930f
Filip Prochazka <filip@thekingdom.nz>
2022-08-03 17:31:11 +0200

    include conmon version

--- containers/podman-ubuntu/conmon/build.sh
+++ containers/podman-ubuntu/conmon/build.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
+set -eux
+
 export PACKAGE_ARCH="$(dpkg --print-architecture)"
 PKG_ROOT="$(pwd)/conmon_${CONMON_VERSION}-1"
 
 mkdir -p $PKG_ROOT/usr/local/libexec/podman
 
-git clone https://github.com/containers/conmon.git
+git clone --depth 1 --branch "v${CONMON_VERSION}" https://github.com/containers/conmon.git
 cd conmon
-git checkout "v${CONMON_VERSION}"
 export GOCACHE="$(mktemp -d)"
 make DESTDIR=$PKG_ROOT podman
 
