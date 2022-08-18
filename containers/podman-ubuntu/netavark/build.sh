cf36ff1de5e0416b35400160f4fff8329c0c2e94
Filip Prochazka <filip@thekingdom.nz>
2022-08-18 16:14:53 +0200

    fix netavark build

--- containers/podman-ubuntu/netavark/build.sh
+++ containers/podman-ubuntu/netavark/build.sh
@@ -3,13 +3,13 @@
 set -eux
 
 PKG_ROOT="$(pwd)/netavark_${NETAVARK_VERSION}"
-mkdir -p "${PKG_ROOT}/usr/local/bin"
+mkdir -p "${PKG_ROOT}/usr/local/libexec/podman"
 
 git clone --depth 1 --branch "v${NETAVARK_VERSION}" https://github.com/containers/netavark/
 
 cd netavark
 make
-make DESTDIR=$PKG_ROOT install
+cp ./bin/netavark "${PKG_ROOT}/usr/local/libexec/podman/"
 cd ..
 
 tar -zcvf /packages/netavark_${NETAVARK_VERSION}_${ARCH}.tar.gz "${PKG_ROOT}/usr"
