92765a08f405b21554424519bee8772f47ebd62e
Filip Prochazka <filip@thekingdom.nz>
2022-09-19 15:30:39 +0200

    improve naming

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -Eeux
 
-if [[ -z "${DEPTHAI_BRANCH}" ]]; then
-  DEPTHAI_BRANCH="main"
-fi
+TAG_SUFFIX=""
 
 if [[ -n "${EXTERNAL_TRIGGER_REF}" ]]; then
   DEPTHAI_BRANCH="${EXTERNAL_TRIGGER_REF##*/}"
+elif [[ -z "${DEPTHAI_BRANCH}" ]]; then
+  DEPTHAI_BRANCH="main"
+else
+  TAG_SUFFIX="${TAG_SUFFIX}-custom"
 fi
 
-TAG_SUFFIX=""
 if [[ "$GITHUB_REF_NAME" != "main" ]]; then
-  TAG_SUFFIX="-unstable"
+  TAG_SUFFIX="${TAG_SUFFIX}-dev"
 fi
 
 IMAGE_VERSION=$(date +"%Y.%j.%H%M")
@@ -24,8 +25,8 @@ cd ..
 BASE_PACKAGE="ghcr.io/luxonis/robothub-app"
 BASE_TAG="${BASE_PACKAGE}:${IMAGE_VERSION}"
 
-BASE_ALPINE_TAG="${BASE_TAG}-alpine${TAG_SUFFIX}"
-BASE_UBUNTU_TAG="${BASE_TAG}-ubuntu${TAG_SUFFIX}"
+BASE_ALPINE_TAG="${BASE_TAG}-alpine3.16${TAG_SUFFIX}"
+BASE_UBUNTU_TAG="${BASE_TAG}-ubuntu22.04${TAG_SUFFIX}"
 
 echo "================================"
 echo "Building images..."
@@ -43,7 +44,15 @@ DOCKER_BUILDKIT=1 docker buildx \
   --platform linux/arm64/v8,linux/amd64 \
   --build-arg "DEPTHAI_VERSION=v${DEPTHAI_VERSION}" \
   --label	"com.luxonis.rh.depthai=${DEPTHAI_VERSION}" \
+  --label	"com.luxonis.rh.depthai.branch=${DEPTHAI_BRANCH}" \
   --label	"com.luxonis.rh.base=alpine" \
+  --label	"org.opencontainers.image.url=https://github.com/luxonis/robothub-sdk" \
+  --label	"org.opencontainers.image.documentation=https://github.com/luxonis/robothub-sdk" \
+  --label	"org.opencontainers.image.source=https://github.com/luxonis/robothub-sdk" \
+  --label	"org.opencontainers.image.version=${IMAGE_VERSION}" \
+  --label	"org.opencontainers.image.vendor=Luxonis" \
+  --label	"org.opencontainers.image.title=RobotHub Perception App Base" \
+  --label "org.opencontainers.image.description=Based on: Alpine\nDepthAI branch: ${DEPTHAI_BRANCH}\nDepthAI version: ${DEPTHAI_VERSION}" \
   -t ${BASE_ALPINE_TAG} \
   --push \
   --file ./robothub_sdk/docker/alpine/Dockerfile \
@@ -60,7 +69,15 @@ DOCKER_BUILDKIT=1 docker buildx \
   --platform linux/arm64/v8,linux/amd64 \
   --build-arg "DEPTHAI_VERSION=v${DEPTHAI_VERSION}" \
   --label	"com.luxonis.rh.depthai=${DEPTHAI_VERSION}" \
+  --label	"com.luxonis.rh.depthai.branch=${DEPTHAI_BRANCH}" \
   --label	"com.luxonis.rh.base=ubuntu" \
+  --label	"org.opencontainers.image.url=https://github.com/luxonis/robothub-sdk" \
+  --label	"org.opencontainers.image.documentation=https://github.com/luxonis/robothub-sdk" \
+  --label	"org.opencontainers.image.source=https://github.com/luxonis/robothub-sdk" \
+  --label	"org.opencontainers.image.version=${IMAGE_VERSION}" \
+  --label	"org.opencontainers.image.vendor=Luxonis" \
+  --label	"org.opencontainers.image.title=RobotHub Perception App Base" \
+  --label "org.opencontainers.image.description=Based on: Ubuntu\nDepthAI branch: ${DEPTHAI_BRANCH}\nDepthAI version: ${DEPTHAI_VERSION}" \
   -t ${BASE_UBUNTU_TAG} \
   --push \
   --file ./robothub_sdk/docker/ubuntu/Dockerfile \
