922d8df83e758365ee6a4f276de10da096406a2f
Filip Peška <56216861+peskaf@users.noreply.github.com>
2022-09-26 20:46:37 +0200

    Typo fix.

--- robothub_sdk/src/robothub_sdk/app.py
+++ robothub_sdk/src/robothub_sdk/app.py
@@ -333,7 +333,7 @@ class App:
                 had_items = False
                 now = time.monotonic()
                 for device in self.devices:
-                    # NOTE: needs depthai>2.17.4
+                    # NOTE: needs depthai>=2.17.4
                     if device.internal.isClosed():
                         raise RuntimeError(f"Device {device.id} / {device.name} disconnected.")
                     last_item = 0
