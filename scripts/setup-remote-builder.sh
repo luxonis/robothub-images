8aa334d931183527089a469e12b0f164dbdb2564
Nikolay Tsoy <nikolay@tsoy.dev>
2023-09-19 11:42:05 +0200

    Format setup-remote-builder.sh script

--- scripts/setup-remote-builder.sh
+++ scripts/setup-remote-builder.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
-
 set -Eeuo pipefail
 
 mkdir -p ~/.ssh/
 touch ~/.ssh/aws_key
-echo "${SSH_KEY}" > ~/.ssh/aws_key
+echo "${SSH_KEY}" >~/.ssh/aws_key
 chmod 600 ~/.ssh/aws_key
 eval $(ssh-agent)
 ssh-add ~/.ssh/aws_key
 
 touch ~/.ssh/config
-echo "Host ${X86_BUILDER_IP}" >> ~/.ssh/config
-echo "  HostName ${X86_BUILDER_IP}" >> ~/.ssh/config
-echo "  User ubuntu" >> ~/.ssh/config
-echo "  StrictHostKeyChecking no" >> ~/.ssh/config
-echo "  IdentityFile /home/runner/.ssh/aws_key" >> ~/.ssh/config
-echo "Host ${ARM_BUILDER_IP}" >> ~/.ssh/config
-echo "  HostName ${ARM_BUILDER_IP}" >> ~/.ssh/config
-echo "  User ubuntu" >> ~/.ssh/config
-echo "  StrictHostKeyChecking no" >> ~/.ssh/config
-echo "  IdentityFile /home/runner/.ssh/aws_key" >> ~/.ssh/config
+echo "Host ${X86_BUILDER_IP}" >>~/.ssh/config
+echo "  HostName ${X86_BUILDER_IP}" >>~/.ssh/config
+echo "  User ubuntu" >>~/.ssh/config
+echo "  StrictHostKeyChecking no" >>~/.ssh/config
+echo "  IdentityFile /home/runner/.ssh/aws_key" >>~/.ssh/config
+echo "Host ${ARM_BUILDER_IP}" >>~/.ssh/config
+echo "  HostName ${ARM_BUILDER_IP}" >>~/.ssh/config
+echo "  User ubuntu" >>~/.ssh/config
+echo "  StrictHostKeyChecking no" >>~/.ssh/config
+echo "  IdentityFile /home/runner/.ssh/aws_key" >>~/.ssh/config
 
 echo "configuring X86 builder at ${X86_BUILDER_IP}"
 docker buildx create \
