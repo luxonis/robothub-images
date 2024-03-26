ac2f5a7b01a5dd161eac326088dbb190bbc52299
petrnovota <petr.novota@luxonis.com>
2024-03-26 15:22:18 +0100

    update robothub version to 2.5.3

--- scripts/build-images-base.sh
+++ scripts/build-images-base.sh
@@ -32,7 +32,7 @@ for elem in "${elems[@]}"; do
         echo "Unknown ROBOTICS_VISION_CORE: ${ROBOTICS_VISION_CORE}"
         continue
     fi
-    ROBOTHUB_VERSION="2.5.2"
+    ROBOTHUB_VERSION="2.5.3"
 
     TAG="${BASE_TAG}-${ROBOTICS_VISION_CORE}-${VARIANT}"
     if [[ "${BASE_IMAGE}" == "ros:humble-ros-core" || "${BASE_IMAGE}" == "ros:humble-ros-base" ]]; then
