fc8295ac6baf1ba4534ad0bbc1686689b53826f9
Jindrich Flidr <jindrich.flidr@luxonis.com>
2025-05-05 00:22:36 +0200

    Add support for visualizer

--- containers/oakapp-base/services/connection-run.sh
+++ containers/oakapp-base/services/connection-run.sh
@@ -3,6 +3,9 @@
 # Get the first non-loopback IP address of the machine (used for local access URL)
 IP=$(hostname -I | awk '{print $1}')
 
+# Set OAKAPP_STATIC_FRONTEND_PORT to 8000 if it's not already defined
+export OAKAPP_STATIC_FRONTEND_PORT="${OAKAPP_STATIC_FRONTEND_PORT:-8000}"
+
 # Helper function to extract a specific attribute from the "data" field of a JSON response
 get_data_attr() {
     echo "$1" | python3.12 -c "import sys, json; print(json.load(sys.stdin)['data']['$2'])"
@@ -55,9 +58,17 @@ write_connection() {
     CLIENT_ID=$(get_data_attr "$CONNECTION_RESPONSE" "client_id")
     TOKEN=$(get_data_attr "$CONNECTION_RESPONSE" "token")
 
+    # Check if OAKAGENT_APP_IDENTIFIER is set to 'com.luxonis.default', fully supported is only viewer. We need to rework this part to fully support static UI.
+    if [[ "$OAKAGENT_APP_IDENTIFIER" == "com.luxonis.default" ]]; then
+        export OAKAGENT_APP_IDENTIFIER="viewer"
+    else
+        export OAKAGENT_APP_IDENTIFIER="visualizer"
+        export OAKAGENT_APP_VERSION="0.10.1"
+    fi
+
     # Print instructions for accessing the DepthAI Viewer remotely and locally
     echo "CONNECTION - To connect DepthAI Viewer remotely, open:"
-    echo "CONNECTION - https://public.luxonis.app/viewer/$OAKAGENT_APP_VERSION/?t=$TOKEN&cid=$CLIENT_ID"
+    echo "CONNECTION - https://public.luxonis.app/$OAKAGENT_APP_IDENTIFIER/$OAKAGENT_APP_VERSION/?t=$TOKEN&cid=$CLIENT_ID"
     echo "CONNECTION - (valid for 15 minutes)"
     echo "CONNECTION - To connect DepthAI Viewer locally, open: https://$IP:$OAKAPP_STATIC_FRONTEND_PORT"
 }
--- containers/oakapp-base/services/nginx-run.sh
+++ containers/oakapp-base/services/nginx-run.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 echo "NGINX - Starting"
 
+# Set OAKAPP_STATIC_FRONTEND_PORT to 8000 if it's not already defined
+export OAKAPP_STATIC_FRONTEND_PORT="${OAKAPP_STATIC_FRONTEND_PORT:-8000}"
+
 # Determine frontend configuration: either serve static files or proxy to backend
 if [ -z "$OAKAPP_STATIC_FRONTEND_PATH" ]; then
     # No static path defined: fallback to reverse proxy
-    export OAKAPP_STATIC_FRONTEND_CONFIGURATION='proxy_pass http://127.0.0.1:8082;'
+    export OAKAPP_STATIC_FRONTEND_CONFIGURATION="
+        proxy_pass http://127.0.0.1:8082;
+        proxy_http_version 1.1;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    "
 else
     # Static path defined: serve frontend files directly
-    export OAKAPP_STATIC_FRONTEND_CONFIGURATION="root $OAKAPP_STATIC_FRONTEND_PATH; index index.html; try_files \$uri \$uri/ /index.html;"
+    export OAKAPP_STATIC_FRONTEND_CONFIGURATION="
+        root $OAKAPP_STATIC_FRONTEND_PATH;
+        index index.html;
+        try_files \$uri \$uri/ /index.html;
+    "
 fi
 
 # Replace placeholders in the NGINX config with env variables (limited to specific ones)
@@ -19,13 +33,12 @@ envsubst '${OAKAPP_STATIC_FRONTEND_CONFIGURATION} ${OAKAPP_STATIC_FRONTEND_PORT}
 mv /tmp/default.tmp /etc/nginx/sites-available/default
 
 # NGINX related configuration files and folders
-
 # Output the final configuration for debugging purposes
 # echo "NGINX - Server configuration:"
 # cat /etc/nginx/sites-available/default
 # echo "NGINX - Global configuration:"
 # cat /etc/nginx/nginx.conf
-
+# 
 # Output Files in frontenf folder
 # echo "Listing contents of $OAKAPP_STATIC_FRONTEND_PATH"
 # if [ -d "$OAKAPP_STATIC_FRONTEND_PATH" ]; then
